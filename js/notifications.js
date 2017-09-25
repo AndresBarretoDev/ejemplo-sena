d1a9c0071c44ad351d0cf5718a82e3e395f8503e
Andrewbarret <andres.barretos24@gmail.com>
2017-09-24 23:11:12 -0500

    ejercicio sena

---
+++ js/notifications.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+		 $.metroLoading({
+		  title: "Loading Recipes",
+		  content:"Chefs cooking",
+		  fa: "fa-refresh",
+		  timeout:4000,
+		  backgroundcontent: "#000000",
+		  opacity:1,
+		  colors:["#0F7549","#149E63","#19C77D"],
+		});
+});
+
+// $(document).ready(function(){
+
+// 		$.smallBox({
+// 		position: 2,
+// 		title:"Bienvenido",
+// 		timeout:8000,
+// 		delay:5,
+// 		colors:["#6D1D99","#0E8699","#099994"],
+// 	});
+// });
+// $(document).ready(function(){
+
+// 	  $.smallBox({
+// 	  position: 4,
+// 	  title:"Espero ayudarte en tu busqueda",
+// 	  timeout:12000,
+// 	  delay:9,
+// 	  colors:["#6D1D99","#0E8699","#099994"],
+// 	});
+// });
